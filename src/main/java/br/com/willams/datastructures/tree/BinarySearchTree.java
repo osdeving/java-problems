11017b61c10e814c337f2389a29068c2b71819e4
osdeving <osdeving@gmail.com>
2022-09-04 04:04:15 -0300

    adicionou busca BST sem recursão

--- src/main/java/br/com/willams/datastructures/tree/BinarySearchTree.java
+++ src/main/java/br/com/willams/datastructures/tree/BinarySearchTree.java
@@ -15,7 +15,7 @@ public class BinarySearchTree {
 		public Node left;
 		public Node right;
 	}
-	
+
 	protected Node root;
 
 	protected boolean nonRecursive = true;
@@ -52,16 +52,16 @@ public class BinarySearchTree {
 
 	public void visit(int type) {
 
-		if(type == LVL) {
+		if (type == LVL) {
 			levelOrder(root);
 			System.out.println();
 			return;
 		}
 
-		if(nonRecursive == true) {
-			if(type == IN)
+		if (nonRecursive == true) {
+			if (type == IN)
 				inOrderNonRecursive(root);
-			else if(type == POS)
+			else if (type == POS)
 				posOrderNonRecursive(root);
 			else
 				visitNode(root, type);
@@ -71,66 +71,68 @@ public class BinarySearchTree {
 		System.out.println();
 	}
 
+	// Time O(n) Space O(n)
 	private void levelOrder(Node root) {
 		Queue<Node> queue = new LinkedList<Node>();
 		Node temp = null;
 
-		if(root == null) return;
+		if (root == null)
+			return;
 
 		queue.add(root);
-		
-		while(!queue.isEmpty()) {
+
+		while (!queue.isEmpty()) {
 			temp = queue.poll();
 
 			System.out.print(temp.key + " ");
 
-			if(temp.left != null) {
+			if (temp.left != null) {
 				queue.add(temp.left);
 			}
 
-			if(temp.right != null) {
+			if (temp.right != null) {
 				queue.add(temp.right);
 			}
 		}
 	}
 
-	// Complexity O(n)  Space O(n)
+	// Time O(n) Space O(n)
 	private void inOrderNonRecursive(Node root) {
 		Stack<Node> stack = new Stack<Node>();
 
-		while(true) {
-			while(root != null) {
+		while (true) {
+			while (root != null) {
 				stack.push(root);
 				root = root.left;
 			}
 
-			if(stack.isEmpty()) {
+			if (stack.isEmpty()) {
 				break;
 			}
 
 			root = stack.pop();
 
 			System.out.print(root.key + " ");
-			
+
 			root = root.right;
 		}
 	}
-	
-	// Complexity O(n)  Space O(n)
+
+	// Time O(n) Space O(n)
 	private void posOrderNonRecursive(Node root) {
 		Stack<Node> stack = new Stack<Node>();
 		Node previous = null;
-		
+
 		do {
-			while(root != null) {
+			while (root != null) {
 				stack.push(root);
 				root = root.left;
 			}
 
-			while(root == null && !stack.isEmpty()) {
+			while (root == null && !stack.isEmpty()) {
 				root = stack.peek();
 
-				if(root.right == null || root.right == previous) {
+				if (root.right == null || root.right == previous) {
 					System.out.print(root.key + " ");
 					stack.pop();
 					previous = root;
@@ -139,22 +141,22 @@ public class BinarySearchTree {
 					root = root.right;
 				}
 			}
-		} while(!stack.isEmpty());
+		} while (!stack.isEmpty());
 	}
-	
-	// Complexity O(n)  Space O(n)
+
+	// Time O(n) Space O(n)
 	void preOrderNonRecursive(Node root) {
 		Stack<Node> stack = new Stack<Node>();
-		while(true) {
-			while(root != null) {
+		while (true) {
+			while (root != null) {
 				System.out.print(root.key + " ");
 				stack.push(root);
 
 				root = root.left;
 			}
 
-			if(stack.isEmpty())
-		 		break;
+			if (stack.isEmpty())
+				break;
 
 			root = stack.pop();
 			root = root.right;
@@ -169,7 +171,7 @@ public class BinarySearchTree {
 			}
 
 			visitNode(node.left, type);
-			
+
 			if (type == IN) {
 				System.out.print(node.key + " ");
 			}
@@ -188,11 +190,11 @@ public class BinarySearchTree {
 
 	public int minNode(Node node) {
 		var curr = node;
-		
+
 		while (curr != null && curr.left != null) {
 			curr = curr.left;
 		}
-		
+
 		return curr.key;
 	}
 
@@ -202,16 +204,41 @@ public class BinarySearchTree {
 
 	protected int maxNode(Node node) {
 		var curr = node;
-		
+
 		while (curr != null && curr.right != null) {
 			curr = curr.right;
 		}
-		
-		return curr.key; 
+
+		return curr.key;
 	}
 
 	public boolean search(int key) {
-		return searchNode(root, key);
+		if(nonRecursive)
+			return searchNodeNonRecursive(root, key);
+		else
+			return searchNode(root, key);
+	}
+
+	
+	//
+	// Nota: percorre a árvore como se fosse uma lista (curr = curr.next) mas decide se 'next' será right ou left
+	// efetuando um possível zigue-zague. 'Possível', porque se a árvore estiver completamente desbalanceada (skew-tree)
+	// de fato será uma lista encadeada ordenada
+	//
+	boolean searchNodeNonRecursive(Node root, int key) {
+		while(root != null) {
+			if(key == root.key) 
+				return true;
+
+			if(key > root.key) 
+				root = root.right;
+			else 
+				root = root.left;
+		}
+
+		// root aqui é igual null
+		// significa que root já chegou null ou percorreu para direita e esquerda sem nunca key==node.key 
+		return false;
 	}
 
 	protected boolean searchNode(Node node, int key) {
@@ -233,22 +260,22 @@ public class BinarySearchTree {
 	}
 
 	protected Node removeNode(Node node, int key) {
-		
-		if(node == null) {
+
+		if (node == null) {
 			return null;
 		}
-		
-		if(key < node.key) {
+
+		if (key < node.key) {
 			node.left = removeNode(node.left, key);
-		} else if(key > node.key) {
+		} else if (key > node.key) {
 			node.right = removeNode(node.right, key);
 		} else {
-			
-			if(node.left == null) {
+
+			if (node.left == null) {
 				return node.right;
 			}
 
-			if(node.right == null) {
+			if (node.right == null) {
 				return node.left;
 			}
 
@@ -260,9 +287,9 @@ public class BinarySearchTree {
 	}
 
 	public static void main(String[] args) {
-		
+
 		var list = List.of(11, 7, 15, 5, 3, 9, 8, 10, 13, 12, 14, 20, 18, 25, 6);
-		
+
 		var bst = new BinarySearchTree();
 		list.stream().forEach(e -> bst.insert(e));
 
@@ -279,54 +306,54 @@ public class BinarySearchTree {
 		bst.visit(BinarySearchTree.LVL);
 
 		System.out.println("\nMenor valor = " + bst.min());
-		System.out.println("\nMaior valor = " + bst.max()); 
-		
-		System.out.print("\nProcurando o valor 10... Valor 10 "+ (bst.search(10) ? "encontrado." : "não encontrado."));
+		System.out.println("\nMaior valor = " + bst.max());
+
+		System.out.print("\nProcurando o valor 10... Valor 10 " + (bst.search(10) ? "encontrado." : "não encontrado."));
 		System.out.print("\nProcurando o valor 4... Valor 4 " + (bst.search(4) ? "encontrado." : "não encontrado."));
-		
+
 		System.out.println("\n\nRemovendo o valor 15...");
-		
+
 		System.out.print("\n\nÁvore antes de remover  ==> ");
 		bst.visit(BinarySearchTree.IN);
-		
+
 		bst.remove(15);
-		
+
 		System.out.print("\nÁvore depois de remover  ==> ");
 		bst.visit(BinarySearchTree.IN);
 	}
 }
 
-/* 
-
-Saída esperada:
-
-$ java BinarySearchTree.java
-
- 
-Visitando PRE ORDER ==> 11 7 5 3 6 9 8 10 15 13 12 14 20 18 25 
-
-
-Visitando IN ORDER  ==> 3 5 6 7 8 9 10 11 12 13 14 15 18 20 25 
-
-
-Visitando POS ORDER ==> 3 6 5 8 10 9 7 12 14 13 18 25 20 15 11 
-
-
-Visitando LEVEL ORDER ==> 11 7 15 5 9 13 20 3 6 8 10 12 14 18 25 
-
-Menor valor = 3
-
-Maior valor = 25
-
-Procurando o valor 10... Valor 10 encontrado.
-Procurando o valor 4... Valor 4 não encontrado.
-
-Removendo o valor 15...
-
-
-Ávore antes de remover  ==> 3 5 6 7 8 9 10 11 12 13 14 15 18 20 25 
-
-Ávore depois de remover  ==> 3 5 6 7 8 9 10 11 12 13 14 18 20 25
-
-
+/*
+ * 
+ * Saída esperada:
+ * 
+ * $ java BinarySearchTree.java
+ * 
+ * 
+ * Visitando PRE ORDER ==> 11 7 5 3 6 9 8 10 15 13 12 14 20 18 25
+ * 
+ * 
+ * Visitando IN ORDER ==> 3 5 6 7 8 9 10 11 12 13 14 15 18 20 25
+ * 
+ * 
+ * Visitando POS ORDER ==> 3 6 5 8 10 9 7 12 14 13 18 25 20 15 11
+ * 
+ * 
+ * Visitando LEVEL ORDER ==> 11 7 15 5 9 13 20 3 6 8 10 12 14 18 25
+ * 
+ * Menor valor = 3
+ * 
+ * Maior valor = 25
+ * 
+ * Procurando o valor 10... Valor 10 encontrado.
+ * Procurando o valor 4... Valor 4 não encontrado.
+ * 
+ * Removendo o valor 15...
+ * 
+ * 
+ * Ávore antes de remover ==> 3 5 6 7 8 9 10 11 12 13 14 15 18 20 25
+ * 
+ * Ávore depois de remover ==> 3 5 6 7 8 9 10 11 12 13 14 18 20 25
+ * 
+ * 
  */
